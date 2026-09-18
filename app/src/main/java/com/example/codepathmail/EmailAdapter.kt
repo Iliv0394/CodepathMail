ebd56735d1bc299c0337100320f40cbe203f9ca7
topkidx <iliv0394@students.vsu.edu>
2026-09-18 09:53:18 -0400

    test 1

---
+++ app/src/main/java/com/example/codepathmail/EmailAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.codepathmail
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class EmailAdapter(
+    private val emails: List<Email>
+) : RecyclerView.Adapter<EmailAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+        val senderTextView: TextView =
+            itemView.findViewById(R.id.senderTextView)
+
+        val titleTextView: TextView =
+            itemView.findViewById(R.id.titleTextView)
+
+        val summaryTextView: TextView =
+            itemView.findViewById(R.id.summaryTextView)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.email_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
+
+        val email = emails[position]
+
+        holder.senderTextView.text = email.sender
+        holder.titleTextView.text = email.title
+        holder.summaryTextView.text = email.summary
+    }
+
+    override fun getItemCount(): Int {
+        return emails.size
+    }
+}
